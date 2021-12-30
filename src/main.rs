6bbab613391b43d07c03c8913f38dd6458cd7fa3
Denys Bohatyrchuk <d.bohatyrchuk@gmail.com>
2021-12-30 17:42:49 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,145 @@
-// Нужно реализовать структуру данных с интерфейсом insert, remove, get (как у коллекции Map), а также добавить поддержку версионирования (вспомни git) со следующим интерфейсом:
+// // Нужно реализовать структуру данных с интерфейсом insert, remove, get (как у коллекции Map), а также добавить поддержку версионирования (вспомни git) со следующим интерфейсом:
 
-// Checkpoint - сохранить текущую версию;
-// Rollback - откатить на определенную версию;
-// Prune - забыть все версии кроме последней.
+// // Checkpoint - сохранить текущую версию;
+// // Rollback - откатить на определенную версию;
+// // Prune - забыть все версии кроме последней.
 
-// use std::collections::HashMap;
-
-
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::hash::{Hash};
 use std::mem;
+use std::fmt::Display;
+// use std::cmp::Ordering;
 
-const INITIAL_Nstore: usize = 1;
+// const initial_ver: &'static str = "0.1";
 
-type Entry<K, V> = Vec<(K, V)>;
-
-pub struct Store<K, V>
-   where
-    K: PartialEq + Clone + Hash,
-    V: Clone, 
-{
-    //   hash_fn: Box<dyn (Fn(&K) -> Option<usize>)>,
-      store: Vec<Entry<K, V>>,
-      pub length: usize,
+pub struct Store<K, V> 
+        where for<'a> K: Hash + Eq + Clone + PartialOrd + Display, 
+            V: Clone {
+    map: HashMap<K, V>,
+    head: Option<K>,
+    observable: Option<V>
 }
 
-// fn bucket<K, V, Q>(store: Box<[Entry<K, V>]>, key: &Q) -> Option<usize>
-// where
-//     K: PartialEq + Clone + Hash,
-//     V: Clone, 
-//     // K: Borrow<Q>,
-//     Q: Hash + Eq + ?Sized + PartialEq + Clone,
-// {
-//     if store.is_empty() {
-//         return None;
-//     }
-//     let mut hasher = DefaultHasher::new();
-//     key.hash(&mut hasher);
-//     Some((hasher.finish() % store.len() as u64) as usize)
-// }
+pub trait Core<K> 
+        where for<'a> K: Hash + Eq + Clone + PartialOrd + Display {
+    fn checkpoint(&mut self);
+    fn rollback(&mut self, key: K);
+    fn prune(&mut self);
+}
 
-impl<K, V> Store<K, V> where K: PartialEq + Clone + Hash, V: Clone {
-
-    pub fn new() -> Self {
+impl<K, V> Store<K, V> 
+    where for<'a> K: Hash + Eq + Clone + PartialOrd + Display + PartialEq + From<String>,
+        V: Clone
+{
+    fn new() -> Self {
         Store {
-            store: Vec::new(),
-            length: 0,
-            // hash_fn: Box::new(bucket)
+            map:  HashMap::new(),
+            head: None,
+            observable: None,
         }
     }
 
-    fn hash<Q>(&self, key: &Q) -> Option<usize>
-    where
-        K: Borrow<Q>,
-        Q: Hash + PartialEq,
-    {
-        if self.store.is_empty() {
-            return None;
+    fn increase_ver(&mut self) -> K {
+        if self.head == None {
+            let new_ver = K::from("0.1".to_string());
+            self.head = Some(new_ver);
+        } else {
+            let current_head = self.head.as_ref().unwrap();
+            let numeric_head = current_head.to_string().parse::<f32>().unwrap_or(0.0);
+            let new_ver = (numeric_head + 0.1).to_string();
+            self.head = Some(K::from(new_ver));
         }
-        let mut hasher = DefaultHasher::new();
-        key.hash(&mut hasher);
-        Some((hasher.finish() % self.store.len() as u64) as usize)
+        self.head.clone().unwrap()
     }
 
-    pub fn insert(&mut self, key: K, value: V) {
-        if self.is_empty() {
-            self.resize();
-        }
-        let idx = self.hash(&key).expect("Store is empty");
-        // let h = (hashcode)(&key);
-        // let idx = h & (self.store.len() - 1);
-        match self.store[idx].iter().position(|e| e.0 == key) {
-            Some(pos) => self.store[idx][pos] = (key, value),
-            None => {
-                self.store[idx].push((key, value));
-                self.length += 1
+    fn ver_list(&self) {
+        for v in self.map.keys() {
+            if Some(v) == self.head.as_ref() {
+                println!("* {}", v);
+                continue;
             }
+            println!("{}", v);
+        }
+        println!("--\\--");
+    }
+
+    fn mutate_observable(&mut self, v: V) {
+        self.observable = Some(v);
+    }
+
+}
+
+impl<K, V> Core<K> for Store<K, V> 
+    where for<'a> K: Hash + Eq + Clone + PartialOrd + Display + From<String>, 
+        V: Clone
+{
+    fn checkpoint(&mut self) {
+        let current_value = mem::replace(&mut self.observable, None);
+        if let Some(value) = current_value {
+            let ver = self.increase_ver();
+            self.map.insert(ver, value);
+        } else {
+            println!("Nothing to checkpoint!");
         }
     }
 
-    pub fn get(&self, key: &K) -> Option<V> {
-        let idx = self.hash(key).expect("Store is empty");
-        // let h = (self.hash_fn)(key); 
-        // let idx = h &  (self.store.len() - 1);
-        self.store[idx]
-        .iter()
-        .find(|e| e.0 == *key)
-        .map(|e| e.1.clone())
-    }
-        
-    pub fn remove(&mut self, key: K) -> Option<V> {
-        let idx = self.hash(&key).expect("Store is empty");
-        // let h = (self.hash_fn)(&key); 
-        // let idx = h & (self.store.len() - 1);
-        match self.store[idx].iter().position(|e| e.0 == key) {
-            Some(pos) => {
-                self.length -= 1;
-                Some(self.store[idx].remove(pos).1)
-            }
-            _ => None,
+    fn rollback(&mut self, rollback_key: K) {
+        if self.map.contains_key(&rollback_key) {
+            let new_list: HashMap<_, _> = self.map
+                    .clone()
+                    .into_iter()
+                    .filter(|(k, _)| k <= &rollback_key)
+                    .collect();
+            let raw_value = self.map.get(&rollback_key).unwrap();
+            self.head = Some(rollback_key);
+            self.observable = Some(raw_value.clone());
+            self.map = new_list;
+            // mem::replace(&mut self.head, Some(rollback_key));
+            // mem::replace(&mut self.observable, Some(raw_value.clone()));
+            // self.map.keys().all(|k| k < rollback_key);
+        } else {
+            println!("Version {} doesnt exist!", rollback_key);
         }
     }
 
-    fn resize(&mut self) {
-        let target_size = match self.store.len() {
-            0 => INITIAL_Nstore,
-            n => 2 * n,
-        };
-
-        let mut new_store = Vec::with_capacity(target_size);
-        new_store.extend((0..target_size).map(|_| Vec::new()));
-
-        for (key, value) in self.store.iter_mut().flat_map(|bucket| bucket.drain(..)) {
-            let mut hasher = DefaultHasher::new();
-            key.hash(&mut hasher);
-            let bucket = (hasher.finish() % new_store.len() as u64) as usize;
-            new_store[bucket].push((key, value));
+    fn prune(&mut self) {
+        if self.map.len() < 2 {
+            println!("Nothing to prune!");
+            ()
         }
-
-        mem::replace(&mut self.store, new_store);
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.length == 0
+        let mut new_map: HashMap<K, V> = HashMap::new();
+        let last_key = self.head.clone().unwrap();
+        let last_value = self.map.get(&last_key).unwrap();
+        new_map.insert(last_key, last_value.clone());
+        self.map = new_map;
     }
 
 }
 
 fn main() {
-    let mut store = Store::new();
-    store.insert("test", 1);
-    let v = store.get(&"test").unwrap();
-    // let m = HashMap::new();
+    let mut store: Store<String, i32> = Store::new();
+    store.mutate_observable(1);
+    store.checkpoint();
+    store.ver_list();
+    println!("Saved first version!");
+    store.mutate_observable(1000);
+    store.checkpoint();
+    store.mutate_observable(1000);
+    store.checkpoint();
+    store.mutate_observable(1000);
+    store.checkpoint();
+    store.ver_list();
+    store.rollback("0.2".to_string());
+    store.ver_list();
+    println!("Rollbacked to 0.2!");
+    store.mutate_observable(1000);
+    store.checkpoint();
+    store.mutate_observable(1000);
+    store.checkpoint();
+    store.mutate_observable(1000);
+    store.checkpoint();
+    store.ver_list();
+    store.prune();
+    store.ver_list();
+    println!("Pruned to the last one!");
 }
